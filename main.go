f72e176ead763647c47ecd8e41edf22dc01e0e65
brian.orwe <brian.orwe@gmail.com>
2025-06-03 21:26:46 +0300

    Setup bubbletea

--- main.go
+++ main.go
@@ -1,4 +1,33 @@
 package main
 
+import (
+	"log"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type Program struct {}
+
+func initProgram()*Program{
+	return &Program{}
+}
+
+func (p Program) Init() tea.Cmd{
+	return nil
+}
+
+func (p Program) Update(msg tea.Msg) (tea.Model, tea.Cmd){
+	return p, nil
+}
+
+
+func (p Program) View() string{
+	return "YO"
+}
+
 func main(){
+	prg := tea.NewProgram(initProgram())
+	if _, err := prg.Run(); err!=nil {
+		log.Fatalln("Error after run: ",err)
+	}
 }
